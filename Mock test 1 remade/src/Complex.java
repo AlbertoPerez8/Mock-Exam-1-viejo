07c87ea76d66e1592f2df6ad898c72d07b5c6758
Alberto J Perez <Alberto.perez8@upr.edu>
2018-09-06 17:20:07 -0400

    Basic barebone code

---
+++ Mock test 1 remade/src/Complex.java
@@ -0,0 +1,34 @@
+
+public class Complex {
+   private double real;
+   private double imaginary;
+   
+    public Complex(double real, double imaginary) {
+    	
+    }
+   
+    public double absolute() {
+    	return 0;   
+
+ }
+    public Complex add(Complex c2) {
+    	return null;
+}
+    public Complex multiply(Complex c2) {
+    	return null;
+    	
+    }
+    public Complex reciprocal() {
+    	return null;
+    }
+    public Complex subtraction(Complex c2) {
+    	return null;
+    }
+    public Complex division(Complex c2) {
+    	return null;
+    }
+    
+    public Complex[] table(Complex c2) {
+    	return null;	
+    }
+}
